84afb55349e595289d07986d497d5b5ddb2ec3be
widal001 <william.daly@mail.goucher.edu>
2020-09-18 21:46:30 -0400

    Outlines first few tests in test_pdf_download.py

---
+++ tests/test_pdf_download.py
@@ -0,0 +1,26 @@
+
+
+class TestParseBOELinks:
+
+    def test_parse_year_links(self):
+        assert 1
+
+    def test_parse_meeting_links(self):
+        assert 1
+
+    def test_parse_meeting_date(self):
+        assert 1
+
+class TestDownloadPDFs:
+
+    def test_download_pdf(self):
+        assert 1
+
+    def test_rename_pdf(self):
+        assert 1
+
+    def test_confirm_all_pdfs_downloaded(self):
+        assert 1
+
+    def test_download_missing_pdfs(self):
+        assert 1
