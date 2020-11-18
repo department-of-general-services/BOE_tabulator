626a3182bae5b083cde12db06254723e51c92089
widal001 <williamdaly422@gmail.com>
2020-11-18 14:53:16 -0500

    Updates the test data to reflect the desired output of get_year_links()

--- tests/data/pdf_download_data.py
+++ tests/data/pdf_download_data.py
@@ -5,15 +5,15 @@ from .sample_boe_page import HTML_TEXT
 SAMPLE_HTML = HTML_TEXT
 
 # Example set of expected year links pulled from HTML_TEXT
-YEAR_LINKS = ['<a href="/minutes-2020">2020</a>',
-              '<a href="/2019">2019</a>',
-              '<a href="/minutes-2018">2018</a>',
-              '<a href="/boe/meetings/minutes">2017</a>',
-              '<a href="/minutes-2016-0">2016</a>',
-              '<a href="/minutes-2015">2015</a>',
-              '<a href="/minutes-2014">2014</a>',
-              '<a href="/minutes-2013">2013</a>',
-              '<a href="/minutes-2012">2012</a>',
-              '<a href="/minutes-2011">2011</a>',
-              '<a href="/minutes-2010">2010</a>',
-              '<a href="/minutes-2009">2009</a>']
+YEAR_LINKS = {'2020': '/minutes-2020',
+              '2019': '/2019',
+              '2018': '/minutes-2018',
+              '2017': '/boe/meetings/minutes',
+              '2016': '/minutes-2016-0',
+              '2015': '/minutes-2015',
+              '2014': '/minutes-2014',
+              '2013': '/minutes-2013',
+              '2012': '/minutes-2012',
+              '2011': '/minutes-2011',
+              '2010': '/minutes-2010',
+              '2009': '/minutes-2009',}
