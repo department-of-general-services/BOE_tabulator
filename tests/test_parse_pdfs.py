7e1e9c8fb8dae2676c1b43371e424ac4cbf5ea20
widal001 <williamdaly422@gmail.com>
2020-11-27 14:40:41 -0500

    Adds test_parse_pages()

--- tests/test_parse_pdfs.py
+++ tests/test_parse_pdfs.py
@@ -19,3 +19,19 @@ class TestMinutes:
 
         assert page_count == minutes.page_count
         assert meeting_date == minutes.meeting_date
+
+
+    @pytest.mark.parametrize(
+        'pdf_path',
+        ['tests/data/2010_03_17.pdf',
+         'tests/data/2013_11_20.pdf']
+    )
+    def test_parse_pages(self, pdf_path):
+
+        path = Path(pdf_path)
+        minutes = Minutes(path)
+
+        minutes.parse_pages()
+        assert minutes.parsed_text is not None
+        print(minutes.parsed_text[:20])
+        assert 0
