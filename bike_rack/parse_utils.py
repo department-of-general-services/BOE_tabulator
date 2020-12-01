ba5d0ec7d519884b53be18693c216cc04be12007
widal001 <williamdaly422@gmail.com>
2020-12-01 09:16:12 -0500

    Adds docstrings to class and moves replace_chars to separate function

--- bike_rack/parse_utils.py
+++ bike_rack/parse_utils.py
@@ -7,7 +7,7 @@ REPLACEMENTS = [
     ('ﬂ', '"'),
     ('™', "'"),
     ('Ł', '•'),
-    (',', "'"),
+    # (',', "'"),
     ('Š', '-'),
     ('€', ' '),
     ('¬', '-'),
@@ -29,7 +29,24 @@ REPLACEMENTS = [
     ('Õ', "'"),
 ]
 
+def replace_chars(text, replacement_list):
+    """Replaces a set of characters specified by a list of replacement keys
+
+    Args:
+        text (str): The raw text whose characters will be replaced
+        replacement_list (list): List of tuples in which the first item
+        is the current character and the second item is its replacement
+    Returns:
+        text (str): The text with the characters replaced
+    """
+    for current, new in replacement_list:
+        text = text.replace(current, new)
+    return text
+
 class Minutes:
+    """Creates an object that represents the minutes for an individual BOE
+    meeting. This object contains the methods used to parse the pdf and
+    stores the outputs of that parsing as a set of attributes"""
 
     def __init__(self, pdf_path):
 
@@ -42,25 +59,49 @@ class Minutes:
 
 
     def read_pdf(self, pdf_path):
+        """Initializes a PdfFileReader instance from the PyPDF2 library that
+        will be called in subsequent methods and attributes
+
+        Args:
+            pdf_path (pathlib.Path): The path to the pdf file to read
+        Returns:
+            reader (PyPDF2.PdfFileReader): Returns an instance of PdfFileReader
+            that will be stored in self.reader
+        """
         file = open(pdf_path, "rb")
         reader = PdfFileReader(file, strict=False)
         return reader
 
 
     def parse_date(self, pdf_path):
+        """Parses a datetime object from the path to the pdf file for use
+        in self.meeting_date and self.year
+
+        Args:
+            pdf_path (pathlib.Path): Path to minutes file passed during
+            instantiation of the Minutes class, expected to end with a
+            */YYYY_MM_DD.pdf pattern
+        Returns:
+            date (datetime.datetime): Returns a datetime object of the
+            date parsed from the pdf_path that will be stored in self.date
+        """
         date_str = pdf_path.stem
         date = dt.datetime.strptime(date_str, "%Y_%m_%d")
         return date
 
 
     def parse_pages(self):
-        text = ""
+        """Extracts text from pdf pages and stores it in self.parsed_text
+
+        Args:
+            self: Uses the self.reader object created by self.read_pdf()
+        Returns:
+            text (str): Returns the text parsed from the pdf pages as a string
+            and also stores that text in self.parsed_text
+        """
+        text_raw = ""
         for page in self.reader.pages:
-            text += page.extractText().strip()
+            text_raw += page.extractText().strip()
+        text = replace_chars(text_raw, REPLACEMENTS)
         self.parsed_text = text
-
-
-    def replace_chars(self, text):
-        for i in REPLACEMENTS:
-            text = text.replace(i[0], i[1])
         return text
