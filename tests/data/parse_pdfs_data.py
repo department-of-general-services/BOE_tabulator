ea701bf56d17d80fc889255d8f4354c4c648a932
widal001 <williamdaly422@gmail.com>
2020-12-01 09:13:41 -0500

    Adds sample text to for test_parse_pages()

--- tests/data/parse_pdfs_data.py
+++ tests/data/parse_pdfs_data.py
@@ -1,4 +1,4 @@
-PARSED _TEXT = """4680  BOARD OF ESITMATES  November 20' 2013 MINUTES  REGULAR MEETING  Honorable Bernard C. "Jack" Young' President Honorable Stephanie Rawlings-Blake' Mayor - ABSENT Harry Black' Director of Finance Honorable Joan M. Pratt' Comptroller and Secretary George A. Nilson' City Solicitor
-Alfred H. Foxx' Director of Public Works David E. Ralph' Deputy City Solicitor Rudolph S. Chow' Deputy Director of Public Works 
-Bernice H. Taylor' Deputy Comptroller and Clerk
-  Pursuant to Article VI' Section 1(c)"""
+PARSED_TEXT = {
+ '2010': '708 BOARD OF ESTIMATES  March 17, 2010  MINUTES   REGULAR MEETING  Honorable Bernard C. "Jack" Young',
+ '2013': '4680  BOARD OF ESITMATES  November 20, 2013 MINUTES  REGULAR MEETING  Honorable Bernard C. "Jack" Yo'
+}
