7963930fbf7e757d8fa114c50c3b5b91ecbd2a76
widal001 <williamdaly422@gmail.com>
2020-12-01 09:17:58 -0500

    Creates a separate test for replace_chars and closes #50

--- tests/test_parse_pdfs.py
+++ tests/test_parse_pdfs.py
@@ -2,16 +2,34 @@ import pytest
 import PyPDF2
 from pathlib import Path
 
-from bike_rack.parse_utils import Minutes
+from bike_rack.parse_utils import Minutes, replace_chars, REPLACEMENTS
 from tests.data.parse_pdfs_data import PARSED_TEXT
+
+
+@pytest.mark.parametrize(
+    'text,expected',
+    [
+        ('March 17, 2010', 'March 17, 2010'),
+        ('ﬁquotesﬂ', '"quotes"')
+    ]
+)
+def test_replace_chars(text, expected):
+    output = replace_chars(text, REPLACEMENTS)
+    print('OUTPUT')
+    print(output)
+    print('EXPECTED')
+    print(expected)
+    assert output == expected
 
 
 class TestMinutes:
 
     @pytest.mark.parametrize(
         'pdf_path,page_count,meeting_date',
-        [('tests/data/2010_03_17.pdf', 105, '2010-03-17'),
-         ('tests/data/2013_11_20.pdf', 209, '2013-11-20')]
+        [
+            ('tests/data/2010_03_17.pdf', 105, '2010-03-17'),
+            ('tests/data/2013_11_20.pdf', 209, '2013-11-20')
+        ]
     )
     def test_init(self, pdf_path, page_count, meeting_date):
 
