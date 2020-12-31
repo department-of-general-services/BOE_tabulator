55d516609e505187049379d211ffb4b315dd85fe
widal001 <williamdaly422@gmail.com>
2020-12-31 11:17:55 -0500

    Writes a failing test for get_meeting_links()

--- tests/scrape/test_get_meeting_links.py
+++ tests/scrape/test_get_meeting_links.py
@@ -1,9 +1,10 @@
 import pytest
+import re
 from bs4 import BeautifulSoup
 from pprint import pprint
 from copy import deepcopy
 
-from tests.scrape.scrape_data import LINKS_2017
+from tests.scrape.scrape_data import HTML_TEXT, LINKS_2017
 
 from common.scrape_utils import get_meeting_links
 
@@ -13,24 +14,22 @@ class TestGetMeetingLinks:
     from the page that lists the BOE minutes for a given year
     """
 
-    def test_standard_link_format(self):
+    def test_get_meeting_links(self):
         """Tests that function parses standard date formats correctly"""
+        # setup
+        expected = LINKS_2017
+        soup = BeautifulSoup(HTML_TEXT, "html.parser")
+        duplicate_dates = soup.find_all("a", text="June 12, 2017")
+        absolute_links = soup.find_all("a", href=re.compile("https://"))
+        assert len(duplicate_dates) > 1
+        assert absolute_links
+        print(duplicate_dates)
+        print(absolute_links)
 
-        assert 1
+        # execution
+        output = get_meeting_links(soup)
+        pprint(output)
 
-    def test_full_url_pdf_link(self):
-        """Tests that function parses standard date formats correctly
-
-        TEST DATA
-        - A sample of the html scraped from a particular page
-        - List of anchor tags for each meeting link in the sample html
-
-        TEST SETUP
-        - Modify the href of one of the anchor tags to in the sample html
-          to reflect the known edge case in the actual BOE meeting links
-          i.e. the href that is a full url instead of a relative reference
-
-        ASSERTIONS
-        - assert that the function returns all of the anchor tags in the list
-        """
-        assert 1
+        # validation
+        assert output == expected
+        assert 0
